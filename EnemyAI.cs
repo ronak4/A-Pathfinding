d084437429044807e65e8fa6ccda1349116e938c
ronak <100610583+ronak4@users.noreply.github.com>
2022-11-25 20:24:47 -0500

    Add files via upload

---
+++ EnemyAI.cs
@@ -0,0 +1,66 @@
+﻿
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform player;
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+    public float sightRange;
+    public bool playerInSightRange;
+    public LayerMask PlayerObj,Ground;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+    
+    private void Update()
+    {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, PlayerObj);
+        
+        if (playerInSightRange) ChasePlayer();
+        if (!playerInSightRange) Patroling();
+    }
+
+    private void Patroling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+        
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+        
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+    }
+    private void SearchWalkPoint()
+    {
+        
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, Ground))
+            walkPointSet = true;
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+    
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, walkPointRange);
+    }
+}
